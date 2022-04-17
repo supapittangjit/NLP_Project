83cc5169a4d8b7a309368fed3c0cbc086b64d34e
it62070068 <56394436+it62070068@users.noreply.github.com>
2022-04-17 16:52:12 +0700

    wd

--- testApi1.py
+++ testApi1.py
@@ -1,15 +1,19 @@
-import requests
+from facepy import GraphAPI
+import json
+
+page_id = "100000822072127"
+access_token = "EAAEblY8Q9B0BABDE1sIQaKqoRMZCrksO1ynzh2D88QF1fhOsX56ZBQFkBdyUHgyWj5AZA4ZC3h2CMDXaIZC7rpndgK7GbZBYxwwRY7Q3uZAu5u9h2ZAOgN4dyi6hghKALvZAVmRDyMAxkrZBEGXbrzAOhSmriqEATiJzpziYtpfhbvzQZDZD"
+
+graph = GraphAPI(access_token)
 
 
-def get_fb_token(app_id, app_secret):
-    url = 'https://graph.facebook.com/oauth/access_token'
-    payload = {
-        'grant_type': 'client_credentials',
-        'client_id': app_id,
-        'client_secret': app_secret
-    }
-    response = requests.post(url, params=payload)
-    return response.json()['access_token']
+data = graph.get(page_id + "/feed", page=True, retry=3,
+                 limit=100, fields='message,likes')
 
 
-print(get_fb_token(555, 1234))
+i = 0
+for p in data:
+    print('Downloading posts', i)
+    with open('facepydata/content%i.json' % i, 'w') as outfile:
+        json.dump(p, outfile, indent=4)
+    i += 1
