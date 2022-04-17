e5d249e47c341a465ddbab78fb403d13a77c6a41
it62070068 <56394436+it62070068@users.noreply.github.com>
2022-04-17 14:21:29 +0700

    1st one

---
+++ main.py
@@ -0,0 +1,15 @@
+import seaborn as sns
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from IPython.display import display
+from library.nullAnalyze import cutNull, checkNull
+
+df = pd.read_csv('netflix_titles.csv')
+# display(df.head())
+
+# cutNull(df)
+checkNull(df)
+# print(df['country'][300])
+# df['first_country'] = df['country'].apply(lambda x: x+2)
+# df['first_country'][300]
